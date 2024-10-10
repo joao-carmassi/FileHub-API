f9cd3fcc3a1d27ea051f0e4815a9957dc0a282d9
Joao Carmassi <joaovitorcarmassi@gmail.com>
2024-10-09 22:33:38 -0300

    adicionando currently

--- star.bat
+++ star.bat
@@ -1,13 +1,5 @@
 :: Comando executa o codigo e mantem o terminal aberto.
 @echo off
 cd "substitua pelo caminho da pasta do backend"
-start cmd /k "node server.js"
-start cmd /k "npm run lt"
-exit
-
-:: Comando executa o codigo em segundo plano.
-@echo off
-cd "substitua pelo caminho da pasta do backend"
-start /B cmd /c "node server.js"
-start /B cmd /c "npm run lt"
+npm run start
 exit
